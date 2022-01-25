5e990b07b070e9c4dcb7f20128586bae39783a72
Pedro-Lopez06 <pedro.lopez3975@alumnos.udg.mx>
2022-01-25 14:06:14 -0600

    activitys nuevos

--- app/src/main/java/lopez/pedro/primeraapp/MainActivity.kt
+++ app/src/main/java/lopez/pedro/primeraapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package lopez.pedro.primeraapp
 
+import android.content.ClipData
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnAbrir:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Pedro Lopez
+        btnAbrir = findViewById(R.id.btnAbrir)
+
+        btnAbrir.setOnClickListener{
+            abrirActivity()
+        }
+    }
+    fun abrirActivity(){
+        val abrirSegunda = Intent(this,SegundaActivity::class.java)
+        startActivity(abrirSegunda)
     }
 }
---
+++ app/src/main/java/lopez/pedro/primeraapp/SegundaActivity.kt
@@ -0,0 +1,11 @@
+package lopez.pedro.primeraapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda)
+    }
+}
